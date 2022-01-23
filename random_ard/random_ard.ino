cf6afafe3ae3451438c8f8947acc208b0cc474b0
jjh4450 <jjh4450jjh4450@gmail.com>
2022-01-23 23:07:26 +0900

    아두이노 변경해봄

--- random_ard/random_ard.ino
+++ random_ard/random_ard.ino
@@ -6,6 +6,9 @@ void setup() {
 void loop() {
   // 0 에서 299 사이 임의 값 출력
   int randNumber = random(700);
-  Serial.println(randNumber);
+  Serial.print(randNumber);
+  Serial.print(",");
+  Serial.print(randNumber);
+  Serial.print("/");
   delay(100);
 }
